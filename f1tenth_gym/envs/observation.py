e945a6e8ff2abc4558b4ab6168e652e3986ead61
Ahmad Amine <AhmadAmine998@gmail.com>
2025-07-11 04:13:31 -0400

    Fix non-direct observations' velocities

--- f1tenth_gym/envs/observation.py
+++ f1tenth_gym/envs/observation.py
@@ -297,8 +297,8 @@ class FeaturesObservation(Observation):
             x, y, theta = std_state[0], std_state[1], std_state[4]
             delta = std_state[2]
             beta = std_state[6]
-            vx = std_state[4] * np.cos(std_state[6])
-            vy = std_state[4] * np.sin(std_state[6])
+            vx = std_state[3] * np.cos(std_state[6])
+            vy = std_state[3] * np.sin(std_state[6])
             angvel = std_state[5]
 
             # create agent's observation dict
